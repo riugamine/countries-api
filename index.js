f78b0f8f25dd479826b6caf0b9b4cac1d64c103f
Jose Angel <jose@techandpeople.com>
2025-01-17 14:47:40 -0400

    Api for countries info

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+const countriesRoutes = require('./routes/countries');
+require ('dotenv').config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//middleware
+app.use(express.json());
+
+//Home route
+app.get('/', (req , res) => {
+    res.send('Country info Backend is running!');
+});
+app.use('/api/countries', countriesRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
+
